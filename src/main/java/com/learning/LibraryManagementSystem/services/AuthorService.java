5dd32433155d638c295f51ad533c8b6b95483448
adarsshsuman333 <adaeshsuman003@gmail.com>
2026-05-07 04:48:13 +0530

    Creating data access and service layer for Author

--- src/main/java/com/learning/LibraryManagementSystem/services/AuthorService.java
+++ src/main/java/com/learning/LibraryManagementSystem/services/AuthorService.java
@@ -1,5 +1,13 @@
 package com.learning.LibraryManagementSystem.services;
 
-public interface AuthorService {
+import com.learning.LibraryManagementSystem.entity.Author;
 
+import java.util.List;
+
+public interface AuthorService {
+    public List<Author> getAllAuthors();
+    public Author getAuthorById(Long id);
+    public void createAuthor(Author author);
+    public void updateAuthor(Author author);
+    public void deleteAuthor(Long id);
 }
--- src/main/java/com/learning/LibraryManagementSystem/services/AuthorServiceImpl.java
+++ src/main/java/com/learning/LibraryManagementSystem/services/AuthorServiceImpl.java
@@ -1,8 +1,37 @@
 package com.learning.LibraryManagementSystem.services;
 
+import com.learning.LibraryManagementSystem.entity.Author;
+import com.learning.LibraryManagementSystem.repository.AuthorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class AuthorServiceImpl implements AuthorService{
+    @Autowired
+    private AuthorRepository authorRepository;
+
+    public List<Author> getAllAuthors(){
+        return authorRepository.findAll();
+    }
+
+    public Author getAuthorById(Long id){
+        Author author = authorRepository.findById(id).orElseThrow(() -> new RuntimeException("Author not found"));
+        return author;
+    }
+
+    public void createAuthor(Author author){
+        authorRepository.save(author);
+    }
+
+    public void updateAuthor(Author author){
+        authorRepository.save(author);
+    }
+
+    public void deleteAuthor(Long id){
+        Author author = authorRepository.findById(id).orElseThrow(() -> new RuntimeException("Author not found"));
+        authorRepository.deleteById(author.getId());
+    }
 
 }
