71e66791a0e7a89696063493e7ee1781996c9a70
adarsshsuman333 <adaeshsuman003@gmail.com>
2026-05-23 20:00:22 +0530

    Creating data access and service layer for Category

--- src/main/java/com/learning/LibraryManagementSystem/services/CategoryService.java
+++ src/main/java/com/learning/LibraryManagementSystem/services/CategoryService.java
@@ -1,4 +1,13 @@
 package com.learning.LibraryManagementSystem.services;
 
+import com.learning.LibraryManagementSystem.entity.Category;
+
+import java.util.List;
+
 public interface CategoryService {
+    public List<Category> getAllCategories();
+    public Category getCategoryById(Long id);
+    public void createCategory(Category category);
+    public void updateCategory(Category category);
+    public void deleteCategory(Long id);
 }
--- src/main/java/com/learning/LibraryManagementSystem/services/CategoryServiceImpl.java
+++ src/main/java/com/learning/LibraryManagementSystem/services/CategoryServiceImpl.java
@@ -1,7 +1,43 @@
 package com.learning.LibraryManagementSystem.services;
 
+import com.learning.LibraryManagementSystem.entity.Category;
+import com.learning.LibraryManagementSystem.repository.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CategoryServiceImpl implements CategoryService{
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Override
+    public List<Category> getAllCategories(){
+        return categoryRepository.findAll();
+    }
+
+    @Override
+    public Category getCategoryById(Long id) {
+        Category category = categoryRepository.findById(id).orElseThrow(() -> new RuntimeException("Category with id " + id + " not found!"));
+        return category;
+    }
+
+    @Override
+    public void createCategory(Category category) {
+        categoryRepository.save(category);
+    }
+
+    @Override
+    public void updateCategory(Category category){
+        categoryRepository.save(category);
+    }
+
+    @Override
+    public void deleteCategory(Long id){
+        Category category = categoryRepository.findById(id).orElseThrow(() -> new RuntimeException("Category with id " + id + " not found!"));
+        categoryRepository.deleteById(id);
+    }
+
 }
